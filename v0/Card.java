b338af5e92276eafe9442397ae2ffd5dc40c4ba3
pserb <paulserbanescu3@gmail.com>
2022-01-14 13:20:47 -0500

    temp

---
+++ v0/Card.java
@@ -0,0 +1,33 @@
+/** Class Card
+ * stores suit, number, and value of a card
+ 
+Ascii art for playing cards:
+
+Suit in the middle:
+♥, ♠, ◆, ♣
+
+Face:
+ ___________
+│ A         │
+│           │
+│           │
+│     ♠     │
+│           │
+│           │
+│         A │
+ ‾‾‾‾‾‾‾‾‾‾‾
+Back:
+ ___________
+│░░░░░░░░░░░│
+│░░░░░░░░░░░│
+│░░░░░░░░░░░│
+│░░░░░░░░░░░│
+│░░░░░░░░░░░│
+│░░░░░░░░░░░│
+│░░░░░░░░░░░│
+ ‾‾‾‾‾‾‾‾‾‾‾
+ */
+
+public class Card {
+    
+}
---
+++ v0/Woo.java
@@ -0,0 +1,17 @@
+public class Woo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            System.out.print(
+                "\n ___________" +
+                "\n│ A         │" +
+                "\n│           │" +
+                "\n│           │" +
+                "\n│     ♠     │" +
+                "\n│           │" +
+                "\n│           │" +
+                "\n│         A │" +
+                "\n ‾‾‾‾‾‾‾‾‾‾‾"
+            );
+        }
+    }
+}
