8eb838dfcb88f0b14f0c8573685e4f9b087934f8
mschneider30 <mschneider30@stuy.edu>
2022-01-18 12:52:19 -0500

    Added files

---
+++ v1/CardArt.java
@@ -0,0 +1,16 @@
+public class CardArt {
+
+  public static void makeCard(String value, String suit) {
+    _data= String[8];
+    _data[0]="_______";
+    _data[1]="| " + value + "    |";
+    _data[2]="|       |";
+    _data[3]="|       |";
+    _data[4]="|   " + suit + "   |"
+    _data[5]="|       |";
+    _data[6]="|       |";
+    _data[7]="|     " + value + " |";
+
+  }
+
+}
