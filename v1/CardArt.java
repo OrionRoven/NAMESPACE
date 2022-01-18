ca28e118280a764f07465adea8d9e02eda169e95
mschneider30 <mschneider30@stuy.edu>
2022-01-18 14:04:13 -0500

    editing art

--- v1/CardArt.java
+++ v1/CardArt.java
@@ -2,17 +2,19 @@ public class CardArt {
 
   public static String[] makeCard(Card input) {
 	if (input.number != 10) {
-    String[] _data= new String[9];
-    _data[0]=" ________ ";
-    _data[1]="| " + input.number + "      |";
-    _data[2]="|        |";
-    _data[3]="|        |";
-    _data[4]="|   " + input.suit + "    |";
-    _data[5]="|        |";
-    _data[6]="|        |";
-    _data[7]="|      " + input.number + " |";
-    _data[8]=" ‾‾‾‾‾‾‾‾ ";
-}
+	    String[] _data= new String[9];
+   	 _data[0]=" ________ ";
+   	 _data[1]="| " + input.number + "      |";
+ 	 _data[2]="|        |";
+    	 _data[3]="|        |";
+   	 _data[4]="|   " + input.suit + "    |";
+   	 _data[5]="|        |";
+   	 _data[6]="|        |";
+   	 _data[7]="|      " + input.number + " |";
+   	 _data[8]=" ‾‾‾‾‾‾‾‾ ";
+	} else {
+	//Copy but change number of spaces
+	}
 
     return _data;
 
